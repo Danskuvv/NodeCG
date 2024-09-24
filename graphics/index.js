73188c1fb39b8b37cb309a0ac9430ace49662a98
Danskuvv <danielvv@metropolia.fi>
2024-09-24 12:21:46 +0300

    bundle

---
+++ graphics/index.js
@@ -0,0 +1,9 @@
+// graphics/index.js
+
+// You can access the NodeCG api anytime from the `window.nodecg` object
+// Or just `nodecg` for short. Like this!:
+nodecg.log.info("Here's an example of using NodeCG's logging API!");
+
+
+
+
